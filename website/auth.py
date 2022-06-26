a3d8bb87d3b8166329ab6cebb9a35f572b780d8f
Rostyslav Marushko <rosmarushko@gmail.com>
2022-06-26 12:27:39 +0300

    change sing up required field notifications

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+
 auth = Blueprint('auth', __name__)
 
 
@@ -43,14 +44,6 @@ def sign_up():
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters', category='error')
         else:
             new_user = User(email=email, name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
